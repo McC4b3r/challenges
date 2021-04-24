f39a7a334420eebfb81814d7ff557461dd176c07
Ryan McCabe <ryan.mccabe089@gmail.com>
2021-04-24 00:01:15 -0700

    completed TP

--- leetcode/returnTheSumOfTwoNumbers.js
+++ leetcode/returnTheSumOfTwoNumbers.js
@@ -12,3 +12,6 @@
 // If you get stuck on a challenge, find help in the Resources tab.
 // If you're really stuck, unlock solutions in the Solutions tab.
 
+const returnSumOfTwoNumbers = (a, b) => {
+  return a + b;
+}
